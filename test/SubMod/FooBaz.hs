4e2a1f5a9e06b9c3ff63cd9d2230a9cdc7c9e3ff
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-26 16:29:06 +0900

    Add comment in Japanese to test multibyte string

--- test/SubMod/FooBaz.hs
+++ test/SubMod/FooBaz.hs
@@ -1,5 +1,8 @@
 module SubMod.FooBaz where
 
+-- マルチバイト文字があるとエラーになるかどうかをテストするためのコメント
+-- Above comment is checking that occur error if contents include multibyte string.
+
 prop_additionCommutative :: Int -> Int -> Bool
 prop_additionCommutative a b = a + b == b + a
 
